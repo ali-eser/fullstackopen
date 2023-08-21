bc4b162ed699c6d8c1956af2b65d1a4a0150f9f3
ali-eser <115903331+ali-eser@users.noreply.github.com>
2023-08-21 19:14:34 +0300

    Finish exercise 2.1

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,3 +1,14 @@
+const Course = ({ course }) => {
+    return (
+        <div>
+            <h1>{course.name}</h1>
+            <p>{course.parts[0].name} {course.parts[0].exercises} </p>
+            <p>{course.parts[1].name} {course.parts[1].exercises} </p>
+            <p>{course.parts[2].name} {course.parts[2].exercises} </p>
+        </div>
+    )
+}
+
 const App = () => {
     const course = {
         id: 1,
