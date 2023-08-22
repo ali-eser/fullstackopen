397aa05b6856415b97f57ee614ba21dc52a72a2b
ali-eser <115903331+ali-eser@users.noreply.github.com>
2023-08-22 20:17:50 +0300

    Finish exercise 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,19 +2,26 @@ import { useState } from 'react'
 
 const App = () => {
     const [persons, setPersons] = useState([
-        { name: 'Arto Hellas' }
+        { name: 'Arto Hellas', number: '040-1234567'}
     ]);
     const [newName, setNewName] = useState('');
+    const [newNumber, setNewNumber] = useState('');
 
     const handleNameChange = (event) => {
         event.preventDefault();
         setNewName(event.target.value);
     };
 
+    const handleNumberChange = (event) => {
+        event.preventDefault();
+        setNewNumber(event.target.value);
+    }
+
     const addPerson = (event) => {
         event.preventDefault();
         const personObject = {
-            name: newName
+            name: newName,
+            number: newNumber
         };
         for (let i = 0; i < persons.length; i++) {
             if (persons[i].name === personObject.name) {
@@ -35,13 +42,17 @@ const App = () => {
                     name: <input onChange={handleNameChange} />
                 </div>
                 <div>
+                    number: <input onChange={handleNumberChange} />
+                </div>
+                <div>
                     <button type="submit">add</button>
                 </div>
                 <div>debug: {newName}</div>
             </form>
             <h2>Numbers</h2>
             <ul>
-                {persons.map(person =>  <li key={person.name}>{person.name}</li>)}
+                {persons.map(person =>
+                    <li key={person.name}>{person.name} {person.number}</li>)}
             </ul>
         </div>
     )
