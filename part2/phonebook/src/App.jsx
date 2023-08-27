c3f60eb088ed331ec00bf48a4063674ffb62a6d1
ali-eser <115903331+ali-eser@users.noreply.github.com>
2023-08-27 22:47:18 +0300

    Finish exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,19 +1,23 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import Filter from "./components/Filter.jsx";
 import PersonForm from "./components/PersonForm.jsx";
 import PeopleList from "./components/PeopleList.jsx";
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-1234567', id: 1, show: true },
-        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2, show: true },
-        { name: 'Dan Abramov', number: '12-43-234345', id: 3, show: true },
-        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4, show: true }
-    ]);
+    const [persons, setPersons] = useState([]);
     const [newName, setNewName] = useState('');
     const [newNumber, setNewNumber] = useState('');
     const [search, setSearch] = useState('');
     const [showAll, setShowAll] = useState(true);
+
+    useEffect(() => {
+        axios
+            .get('http://localhost:3001/persons')
+            .then(response => {
+                setPersons(response.data)
+            })
+    }, [])
 
     const handleNameChange = (event) => {
         event.preventDefault();
