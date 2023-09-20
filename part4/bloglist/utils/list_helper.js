b9148774415f8b3644bc68155143f8ba65dd931e
ali-eser <115903331+ali-eser@users.noreply.github.com>
2023-09-20 21:39:34 +0300

    Finish exercise 4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,19 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  if (blogs.length === 1) {
+    return blogs[0].likes
+  } else {
+    let likesCount = 0
+    for (let i = 0; i < blogs.length; i++) {
+      likesCount += blogs[i].likes
+    }
+    return likesCount
+  }
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
