a497477c4e6e3a87a8aefc12e1977bc3a543183e
ali-eser <115903331+ali-eser@users.noreply.github.com>
2023-11-23 23:53:45 +0300

    Update blog_app.cy.js

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,11 +1,32 @@
 describe('Blog app', function() {
   beforeEach(function() {
-    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    /*cy.request('POST', 'http://localhost:3003/api/testing/reset')*/
     cy.visit('http://localhost:5173')
   })
 
   it('login form is shown', function() {
     cy.contains('Login')
     cy.contains('Username:')
+  })
+})
+
+describe('Login', function() {
+  it('succeeds with correct credentials', function() {
+    cy.login({ username: 'root', password: 'ali' })
+    cy.contains('Superuser logged in')
+  })
+
+  it('fails with wrong credentials', function() {
+    cy.request({
+      method: 'POST',
+      url: 'http://localhost:3003/api/login',
+      failOnStatusCode: false,
+      auth: {
+        username: 'wrong',
+        password: 'wrong'
+      }
+    })
+    cy.get('.notification')
+      .should('contain', 'wrong username or password!')
   })
 })
