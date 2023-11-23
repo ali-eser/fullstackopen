629c9e7b9642e0c25a23c666cef826640d4da43e
ali-eser <115903331+ali-eser@users.noreply.github.com>
2023-11-23 21:25:35 +0300

    Finish exercise 5.17

---
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -0,0 +1,11 @@
+describe('Blog app', function() {
+  beforeEach(function() {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    cy.visit('http://localhost:5173')
+  })
+
+  it('login form is shown', function() {
+    cy.contains('Login')
+    cy.contains('Username:')
+  })
+})
